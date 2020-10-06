bfb1ed2b06bdbe2057630e31e1f4e09794f96f45
EVANDRO <67401043+Evandro-git20@users.noreply.github.com>
2020-10-06 09:12:24 -0300

    Update Questao 02.py

--- Atividade 03/Questao 02.py
+++ Atividade 03/Questao 02.py
@@ -7,6 +7,12 @@ time = str(input('Digite o nome do time: '))
 vitorias = int(input('Digite o número de vitórias: '))
 derrotas = int(input('Digite o número de derrotas: '))
 empates = int(input('Digite o número de empates: '))
+
+pts_ganhos = vitorias * 3 + empates
+pts_perdidos = derrotas * 3 + empates * 2
+total_pts = pts_ganhos + pts_perdidos
+aproveitamento = (pts_ganhos/total_pts) * 100
+
 print(f'{time}:')
 print(f'Pontos ganhos: {pts_ganhos}')
 print(f'Pontos perdidos: {pts_perdidos}')
